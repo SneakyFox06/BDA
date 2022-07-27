5c8a472f7800a3a1d95afcf68bfe695e5e540e7b
SneakyFox06 <deshansaparamadu3@gmail.com>
2022-07-27 13:40:56 +0530

    update

---
+++ app/src/main/java/com/dsteksoftwares/bda/Email/Util.java
@@ -0,0 +1,6 @@
+package com.dsteksoftwares.bda.Email;
+
+public class Util {
+    public static final String EMAIL = "lifesharedstek@gmail.com";
+    public static final String PASSWORD = "fheoyilarridhcxh";
+}
